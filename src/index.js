266bd7f660850795a8deb2e3b6fe17d54ea19174
Oleg Koleda <olegator2510@mail.ru>
2018-02-23 15:12:44 +0300

    answer

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 class Sorter {
   constructor() {
     this.arr = [];
+    this.compareFunction = function (a, b) {
+      if (a > b) return 1;
+      if ( a < b) return -1;
+      if ( a == b) return 0;
+    };
   }
 
   add(element) {
@@ -20,11 +25,33 @@ class Sorter {
   }
 
   sort(indices) {
-    // your implementation
+    var idndexArray = indices.sort(function (a, b) {
+      return a - b;
+  });
+
+    if (idndexArray.length <= 1) return this.arr;
+
+    var n = idndexArray[idndexArray.length - 1];
+    
+    for (var i = idndexArray[0]; i < n; i++) {
+        var min = i;
+
+        for (var j = i + 1; j <= n; j++) {
+          if ( this.compareFunction(this.arr[j], this.arr[min] ) < 0) 
+          // if ( this.arr[j] < this.arr[min] ) 
+               min = j;
+        }
+
+    var temp = this.arr[min]; 
+    this.arr[min] = this.arr[i];
+    this.arr[i] = temp;
+    }
+
+   // return this.arr;
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.compareFunction = compareFunction;
   }
 }
 
